ef207c6dc262460637adfbf1bfe27dd67dd9b7b1
rahknov <vonkharjicantubat@gmail.com>
2024-09-03 23:30:01 +0800

    Updated Directory of Images

--- view/gallery.php
+++ view/gallery.php
@@ -14,61 +14,61 @@
     <div class="gallery-flex-container">
         <div class="gallery">
             <a href="#"> 
-                <img src="../images/goldenret.jpg" alt="Golden Retriever">
+                <img src="images/goldenret.jpg" alt="Golden Retriever">
             </a>
             <div class="description">Golden Retriever</div>
         </div>
         <div class="gallery"> 
             <a href="#">
-                <img src="../images/corgi.jpg" alt="Corgi">
+                <img src="images/corgi.jpg" alt="Corgi">
             </a>
             <div class="description">Corgi</div>
         </div>
         <div class="gallery"> 
             <a href="#"> 
-                <img src="../images/bulldog.jpg" alt="Bulldog">
+                <img src="images/bulldog.jpg" alt="Bulldog">
             </a>
             <div class="description">Bulldog</div>
         </div>
         <div class="gallery"> 
             <a href="#"> 
-                <img src="../images/pom.jpg" alt="Pomeranian">
+                <img src="images/pom.jpg" alt="Pomeranian">
             </a>
             <div class="description">Pomeranian</div>
         </div>
         <div class="gallery"> 
             <a href="#">
-                <img src="../images/german-shepherd.jpg" alt="German Shepherd">
+                <img src="images/german-shepherd.jpg" alt="German Shepherd">
             </a>
             <div class="description">German Shepherd</div>
         </div>
         <div class="gallery"> 
             <a href="#">
-                <img src="../images/beagle.jpg" alt="Beagle">
+                <img src="images/beagle.jpg" alt="Beagle">
             </a>
             <div class="description">Beagle</div>
         </div>
         <div class="gallery"> 
             <a href="#">
-                <img src="../images/poodle.jpg" alt="Poodle">
+                <img src="images/poodle.jpg" alt="Poodle">
             </a>
             <div class="description">Poodle</div>
         </div>
         <div class="gallery"> 
             <a href="#"> 
-                <img src="../images/rottweiler.jpg" alt="Rottweiler">
+                <img src="images/rottweiler.jpg" alt="Rottweiler">
             </a>
             <div class="description">Rottweiler</div>
         </div>
         <div class="gallery"> 
             <a href="#">
-                <img src="../images/chow.jpg" alt="Chow Chow">
+                <img src="images/chow.jpg" alt="Chow Chow">
             </a>
             <div class="description">Chow Chow</div>
         </div>
         <div class="gallery"> 
             <a href="#">
-                <img src="../images/husky.jpg" alt="Siberian Husky">
+                <img src="images/husky.jpg" alt="Siberian Husky">
             </a>
             <div class="description">Siberian Husky</div>
         </div>
